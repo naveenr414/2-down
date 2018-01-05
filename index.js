8d57dd842bccbe94659010f45e3b5d78f4d5e5a5
naveenr414 <dsfan414@gmail.com>
2018-01-04 22:15:52 -0500

    Allowed for selection of puzzles

---
+++ index.js
@@ -0,0 +1,36 @@
+var app = require('express')();
+var express = require("express");
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var fs = require('fs');
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.use('/', express.static(__dirname + '/'));
+
+io.on('connection', function(socket){	
+	socket.on('entered', function(msg){
+		console.log(msg);
+		io.emit('entered',msg);
+	}); 
+	
+	socket.on('request', function(msg){
+		var location = 'nyt/2011/'+msg+".json";
+		console.log("Reading from "+location);
+		
+		fs.readFile(location, 'utf8', function(err, contents) {
+			data = JSON.parse(contents);
+			console.log("Data "+data);
+			io.emit('metadata',data);
+		});
+		
+	});
+});
+
+
+
+http.listen(process.env.PORT || 3000, function(){
+  console.log('listening on *:3000');
+});
